390f7c9610c9ad775824c8ab32a0377c16bdebfa
98SED <red_g4898@outlook.com>
2021-09-08 21:09:44 -0500

    sep09

---
+++ Metodologia_de_Programacion_Git/sep07/func_mat.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //Declaración de variable tip double
+    double raiz = 0, operando = 7364.837; //Double a diferencia de float puede contener una mayor cantidad de digitos en la parte fraccionaria. Es posible declarar más de una variable de un mismo tipo de dato.
+    float total_compra = 0;
+    float precio_articulo = 0;
+    float tasa_iva = 0.16;
+
+    precio_articulo = 37.8;
+    total_compra = precio_articulo * 10 * (1 + tasa_iva);
+
+    printf("El total de la compra es %lf\n", total_compra);
+    raiz = sqrt(operando);
+    printf("El resultado de la raíz cuadrada de %lf es: %lf\n", operando, raiz);
+
+    return 0;
+}
---
+++ Notas.md
@@ -0,0 +1,18 @@
+ssh -i rdelgadillo_key.pem rdelgadillo@ec2-54-158-217-0.compute-1.amazonaws.com -o ServerAliveInterval=30
+chmod 600 rdelgadillo_key.pem
+
+gcc nombreprograma.c -o nombreprograma
+gcc nombreprograma.c -o nombreprograma -lm
+
+./nombreprograma
+
+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
+
+git init
+git status
+git remote add nombrecorto url
+
+git add archivo.extension
+git commit -m 'mensaje'
+git push -u mptsusi main
+
