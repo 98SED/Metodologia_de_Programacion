b84b449bdc77f201ad154427277cf1253f0f1560
98SED <red_g4898@outlook.com>
2021-11-09 16:19:49 -0600

    09112021

--- tareas/sep/tarea_04_01.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    printf("\nAlumno: Rodrigo Eduardo Delgadillo González\nFecha: 14-09-2021\nActividad: tarea_04_01.c\n");
-    printf("\n*\n**\n***\n****\n*****\n\n");
-
-    return 0;
-}
